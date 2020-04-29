a73d810c1725cffd3e8a75455295d58fc49050b7
Antwone <Antwone@gmail.com>
2020-04-29 12:51:04 -0400

    added list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =
+[
+  {id: 11, name: 'Antwone'},
+  {id: 12, name: 'Dr. Magnificent'},
+  {id: 13, name: 'Mr. Terrific'},
+  {id: 14, name: 'Mr. Fantastic'}
+]
